70ab2ff60a23d4a190c5a06091e8f4128e65e4d9
Chandana <srujana988966@gmail.com>
2026-05-18 21:51:59 +0530

    Fixed brute force feature

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
+# Caesar Cipher Function
 def caesar_cipher(text, shift, mode):
     result = ""
+
     if mode == "decrypt":
         shift = -shift
 
@@ -16,17 +19,39 @@ def caesar_cipher(text, shift, mode):
 
     return result
 
+
+# Brute Force Function
+def brute_force(text):
+    results = []
+    for shift in range(26):
+        decrypted = caesar_cipher(text, shift, "decrypt")
+        results.append(f"Shift {shift}: {decrypted}")
+    return results
+
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     output = ""
+    brute_results = []
+
     if request.method == "POST":
         text = request.form["text"]
-        shift = int(request.form["shift"])
         mode = request.form["mode"]
 
-        output = caesar_cipher(text, shift, mode)
+        # ONLY take shift if needed
+        if mode == "bruteforce":
+            brute_results = brute_force(text)
+        else:
+            try:
+                shift = int(request.form["shift"])
+            except:
+                shift = 0
 
-    return render_template("index.html", output=output)
+            output = caesar_cipher(text, shift, mode)
+
+    return render_template("index.html", output=output, brute_results=brute_results)
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
